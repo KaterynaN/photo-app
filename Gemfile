c6b021fd641e60671ba9d78709124dfbb506bd57
KaterynaN <kateryna.od.ua@gmail.com>
2019-07-22 14:11:03 +0300

    Prod file upload

--- Gemfile
+++ Gemfile
@@ -39,7 +39,7 @@ gem 'font-awesome-rails'
 gem 'stripe'
 gem 'carrierwave'
 gem 'mini_magick'
-gem 'fog'
+gem 'fog-aws'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,10 +1,11 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
-        :provider => 'AWS',
+        :provider => 'fog/aws',
         :aws_access_key_id => ENV['AWSAccessKeyID'],
         :aws_secret_access_key => ENV['AWSSecretKey']
     }
     config.fog_directory = ENV['S3_BUCKET']
+    config.storage = :fog
   end
 end
